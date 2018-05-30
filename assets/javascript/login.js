b124f983645ac83659936f59e191b059eba521fa
Chatura Ahangama <chaturadroid@gmail.com>
2018-05-30 11:09:11 -0700

    update with note

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 //Notify the browser
-notifyCurrentBrowser()
+//notifyCurrentBrowser()
 
 //This is use to submit a user name and password
 $("#button-submit").click(function () {
