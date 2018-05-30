6e3eca4dbc65d390dd01f4c0cac9114fa38d188f
Chatura Ahangama <chaturadroid@gmail.com>
2018-05-30 10:57:49 -0700

    Safari compatibility

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -1,34 +1,34 @@
 
 // have to initialize first for modals to work
-$('.modal').modal(); 
+$('.modal').modal();
 
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.fixed-action-btn');
     var instances = M.FloatingActionButton.init(elems, {
-      direction: 'left'
+        direction: 'left'
     });
-  });
-       
+});
+
+//Notify the browser
+notifyCurrentBrowser()
+
 //This is use to submit a user name and password
 $("#button-submit").click(function () {
     var txtEmail = $("#email_inline").val();
     var txtPassword = $("#password").val();
-   
-   //Simple validation
-    if(txtEmail.length >= 0)
-    {
-        if( /(.+)@(.+){2,}\.(.+){2,}/.test(txtEmail) ){
+
+    //Simple validation
+    if (txtEmail.length >= 0) {
+        if (/(.+)@(.+){2,}\.(.+){2,}/.test(txtEmail)) {
             // valid email so do nothing it can continue
-          } else {
+        } else {
             // invalid email
             return displayModal("Invalid Email")
-          }
-       
+        }
+
     }
 
-    if(txtPassword.length < 6)
-    {
+    if (txtPassword.length < 6) {
         return displayModal("Invalid Password Length")
     }
 
@@ -36,7 +36,7 @@ $("#button-submit").click(function () {
     const promise = auth.signInWithEmailAndPassword(txtEmail, txtPassword);
 
     //On fail it will display the fail error
-    promise.catch(function(error){displayModal(error.message)});
+    promise.catch(function (error) { displayModal(error.message) });
 });
 
 //On a the button click to signup
@@ -45,26 +45,24 @@ $("#button-signup").click(function () {
     var txtEmail = $("#email_inline").val();
     var txtPassword = $("#password").val();
 
-    if(txtEmail.length == 0 || txtPassword < 6)
-    {
+    if (txtEmail.length == 0 || txtPassword < 6) {
         //Some UI info for the user
         displayModal("Make Sure to Fill in the information highlighted");
         $("#passwordRow").delay("4000").css('background-color', '#ee6e73').fadeIn();
         $("#emailRow").delay("4000").css('background-color', '#ee6e73').fadeIn();
-        $(".input-field>label").css('color','white');
+        $(".input-field>label").css('color', 'white');
         return;
     }
     const auth = firebase.auth();
     console.log(txtEmail, txtPassword);
     const promise = auth.createUserWithEmailAndPassword(txtEmail, txtPassword);
-    promise.catch(function(error){displayModal(error.message)});
+    promise.catch(function (error) { displayModal(error.message) });
 });
 
 //Multi Purpose message Loader
-function displayModal(message)
-{
-  $('#modal-message').html(message);  
-  $('#modal1').modal('open'); 
+function displayModal(message) {
+    $('#modal-message').html(message);
+    $('#modal1').modal('open');
 }
 
 //Log-out button can log us out
@@ -73,44 +71,43 @@ $("#logout-now").click(function () {
 });
 
 //This is for the google Auth
-$("#signin-google").click(function () 
-{
-//For Google Auth
-var provider = new firebase.auth.GoogleAuthProvider();
+$("#signin-google").click(function () {
+    //For Google Auth
+    var provider = new firebase.auth.GoogleAuthProvider();
 
-//Fire base event to trigger google popup
-firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // ...
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
- });
+    //Fire base event to trigger google popup
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
 });
 
 
 //Firebase event handler that asynch updates our session based on the login status both internal and google auth
 firebase.auth().onAuthStateChanged(firebaseUser => {
     if (firebaseUser) {
-        console.log("User is Logged in"); 
-       
+        console.log("User is Logged in");
+
         $("#dynamicMenu").removeClass("hide");
- 
-        localStorage.setItem("user-logged",true);
+
+        localStorage.setItem("user-logged", true);
     }
     else {
         console.log("User is Not-Logged in");
-        localStorage.setItem("user-logged",false);
-    }   
+        localStorage.setItem("user-logged", false);
+    }
     IsLoggedIn();
 
 });
