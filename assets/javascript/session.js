a1a1cb2e92db39251a6919aa1fbc8eed76b4e0ee
Chatura Ahangama <chaturadroid@gmail.com>
2018-05-30 17:41:54 -0700

    update

--- assets/javascript/session.js
+++ assets/javascript/session.js
@@ -10,7 +10,7 @@ function IsLoggedIn() {
     var currentPath = getCurentFileName();
     var loginStatus = localStorage.getItem("user-logged");
     //If not logged in
-    if (loginStatus == "false" )
+    if (loginStatus == "false" || loginStatus == nul)
         switch (currentPath) {
             case "login.html":
                 {
