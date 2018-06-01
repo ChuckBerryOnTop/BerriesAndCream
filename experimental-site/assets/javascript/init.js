96d01ca111f64488e392cfd8253b4cd045f5a877
Chatura Ahangama <chaturadroid@gmail.com>
2018-05-31 18:24:47 -0700

    update this have experimental

--- experimental-site/assets/javascript/init.js
+++ experimental-site/assets/javascript/init.js
@@ -110,3 +110,8 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
 
     }
 });
+
+
+window.location.hash="no-back-button";
+window.location.hash="Again-No-back-button";//again because google chrome don't insert first hash into history
+window.onhashchange=function(){window.location.hash="no-back-button";}
